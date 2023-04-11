eee262b99e333cccfe2d1dcd256c997788299657
YujinOk <yujinok63@gmail.com>
2023-04-11 22:03:37 +0900

    movie cards have been added

--- fixx-app/js/script.js
+++ fixx-app/js/script.js
@@ -1,14 +1,86 @@
+// import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+// dotenv.config();
 const global = {
   currentPage: window.location.pathname,
+};
+
+const displayPopularMovies = async () => {
+  const { results } = await fetchAPIData('movie/popular');
+  // console.log(results);
+  results.forEach((movie) => {
+    console.log(movie);
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <a href="movie-details.html?id=${movie.id}">
+   ${
+     movie.poster_path
+       ? `<img
+    src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+    class="card-img-top"
+    alt=${movie.title}
+  />`
+       : `<img
+  src="../images/no-image.jpg"
+  class="card-img-top"
+  alt="Movie Title"
+/>`
+   }
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">${movie.title}</h5>
+      <p class="card-text">
+        <small class="text-muted">Release: ${movie.release_date}</small>
+      </p>
+    </div>
+    `;
+    document.querySelector('#popular-movies').appendChild(div);
+  });
+};
+// Fetch data from TMDB API
+const fetchAPIData = async (endpoint) => {
+  const API_KEY = 'd435e604c20c204c8e631e9eeb8357bd';
+  const API_URL = 'https://api.themoviedb.org/3/';
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+  // console.log(response.json());
+  const data = await response.json();
+  return data;
+};
+
+// Highlight actice link
+const highlightActiveLink = () => {
+  const links = document.querySelectorAll('.nav-link');
+  links.forEach((link) => {
+    if (link.getAttribute('href') === global.currentPage) {
+      link.classList.add('active');
+    }
+  });
 };
 console.log(window.location.pathname);
 // Init App
 const init = () => {
   switch (global.currentPage) {
     case '/':
-      console.log('Home');
+    case '/index.html':
+      displayPopularMovies();
+      break;
+    case '/shows.html':
+      console.log('Shows');
+      break;
+    case '/movie-details.html':
+      console.log('Movie Details');
+      break;
+    case '/tv-details.html':
+      console.log('TV Details');
+      break;
+    case '/search.html':
+      console.log('Search');
       break;
   }
+  highlightActiveLink();
 };
 
 document.addEventListener('DOMContentLoaded', init);
